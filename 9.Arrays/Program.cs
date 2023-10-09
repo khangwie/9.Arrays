a41375cc6215695895f74d595e45cff973d4f6cd
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-09 13:20:03 +0200

    Add project files.

---
+++ 9.Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //oneWayAttribute way of creating an array
+            int[] luckyNumnber = { 4, 5, 6, 7, };
+            Console.WriteLine(luckyNumnber[0]);
+            //if you wanna populate it later, the 5 is the size
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[1] = "John";
+            friends[2] = "Peter";
+            Console.WriteLine(friends[0]);
+            Console.ReadLine();
+
+        }
+    }
+}
